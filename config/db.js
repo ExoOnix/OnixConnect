76e584da4e2faeca97e7533afdb36e73db74d1f4
marksh16 <mshapirovskyy@gmail.com>
2023-05-23 20:37:16 +0300

    Added routes for postman and connected DB.

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const config = require('config')
+
+const db = config.get('mongoURI')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db)
+
+        console.log('MongoDB Connected...')
+    } catch(err) {
+        console.error(err.message);
+        // Exit process with error code 1(Failure)
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
-
+const connectDB = require('./config/db')
 
 const app = express()
+
+//connect to db
+
+connectDB();
+
 
 app.get('/', (req, res) => {
     res.send("API SENT on /")
@@ -9,7 +14,11 @@ app.get('/', (req, res) => {
 
 
 
-
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 
 const PORT = process.env.PORT || 8080;
