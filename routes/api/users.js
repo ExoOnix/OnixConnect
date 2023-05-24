81fb79dc4d007579424918c8a0e85a120e5ffb01
marksh16 <mshapirovskyy@gmail.com>
2023-05-24 10:49:03 +0300

    users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -20,7 +21,7 @@ router.post('/', [
         return res.status(400).json({ errors: errors.array() })
     }
 
-    const { name, email, password } = req.body()
+    const { name, email, password } = req.body
 
     try {
         
@@ -40,13 +41,24 @@ router.post('/', [
         d: 'mm'
     })
 
+    user = new User({
+        name,
+        email,
+        avatar,
+        password
+    })
 
 
     // Encrypt password
+    const salt = await bcrypt.genSalt(10)
+
+    user.password = await bcrypt.hash(password, salt)
+
+    await user.save()
 
 
     // return json tocken
-    res.send('User route')
+    res.send('User Registered')
 
 
     } catch(err) {
