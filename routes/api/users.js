3189400fce92976e02b527c81da9bfab029eb5ea
marksh16 <mshapirovskyy@gmail.com>
2023-05-24 07:42:02 +0300

    User validation.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
+
+const User = require('../../models/User')
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -10,13 +13,41 @@ router.post('/', [
         .isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more charecters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
 
+    const { name, email, password } = req.body()
+
+    try {
+        
+    // send if user exists
+
+    let user = await User.findOne({ email })
+
+    if(user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+    }
+
+    // Get users gravatar
+
+
+    // Encrypt password
+
+
+    // return json tocken
     res.send('User route')
+
+
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
+
+
+
 });
 
 module.exports = router
