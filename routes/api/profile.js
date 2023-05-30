80621b68ba0a5bbeb36342ba0b0f25a58998b178
marksh16 <mshapirovskyy@gmail.com>
2023-05-30 10:04:57 +0300

    Aded github intergration to backend.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
+const request = require('request')
+const config = require('config')
 
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -294,6 +296,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         res.status(500).send('Server error')
     }
 })
+
+// @route   DELETE api/profile/github/:username
+// @desc    Get github repos.
+// @access  Public
+router.get('/github/:username', (req, res) => {
+try {
+    const options = {
+        uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}$client_secret=${config.get('githubSecret')}`,
+        method: 'GET',
+        headers: { 'user-agent': 'node.js' }
+    }
+
+    request(options, (error, response, body) => {
+        if(error) console.log(error)
+
+        if(response.statusCode !== 200 ) {
+            return res.status(404).json({ msg: 'No github profile found' })
+        }
+
+        res.json(JSON.parse(body))
+    })
+
+
+
+} catch(err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+}
+})
+
 
 
 
