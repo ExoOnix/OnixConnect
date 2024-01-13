42a72a6ee52f1f15848fcaad77a59609b007f992
ExoticOnix <mshapirovskyy@gmail.com>
2024-01-13 21:06:52 +0000

    Added alerts with redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 import Register from "./components/auth/Register";
 
 // Redux
@@ -18,6 +19,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
